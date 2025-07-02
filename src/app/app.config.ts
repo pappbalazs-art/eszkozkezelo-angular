642a0541152d67c4fdc1c2dd4d93156c30e47b79
Papp Balázs <hello@pappbalazs.com>
2025-07-02 15:48:24 +0200

    Added firebase to the project.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'eszkozkezelo-5b8e1',
+        appId: '1:151245176229:web:2cad79980ee482ba5a7c4c',
+        storageBucket: 'eszkozkezelo-5b8e1.firebasestorage.app',
+        apiKey: 'AIzaSyBxCn1ZR8ovCFZ0OQ9XJ1VBlYGKKz9fPlQ',
+        authDomain: 'eszkozkezelo-5b8e1.firebaseapp.com',
+        messagingSenderId: '151245176229',
+        measurementId: 'G-KVB8BYPQ49',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
