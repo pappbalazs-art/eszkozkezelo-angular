d34de087b34f49a9eade74e7c550a244ab8ff0cb
Papp Balázs <hello@pappbalazs.com>
2025-07-02 15:54:11 +0200

    Remove firebase config.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,22 +4,13 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environments';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'eszkozkezelo-5b8e1',
-        appId: '1:151245176229:web:2cad79980ee482ba5a7c4c',
-        storageBucket: 'eszkozkezelo-5b8e1.firebasestorage.app',
-        apiKey: 'AIzaSyBxCn1ZR8ovCFZ0OQ9XJ1VBlYGKKz9fPlQ',
-        authDomain: 'eszkozkezelo-5b8e1.firebaseapp.com',
-        messagingSenderId: '151245176229',
-        measurementId: 'G-KVB8BYPQ49',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
