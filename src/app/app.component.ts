ee3240b0700314baf4ead948b37455a1d498d400
Papp Balázs <hello@pappbalazs.com>
2025-07-10 17:40:46 +0200

    Refactor the layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 
 @Component({
-  selector: 'ek-root',
+  selector: 'div.wrapper',
   templateUrl: './app.component.html',
   imports: [RouterOutlet, NavbarComponent],
 })
